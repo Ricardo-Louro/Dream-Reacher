55481b90b9692210ca111b529209601f5545c860
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-08-16 22:43:03 +0100

    Updated Main Menu Manager to unlock and show cursor. Implement portal system as a prefab

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
