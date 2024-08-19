a33afb8c5f465fe70370570c647f0f1361ea195b
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-08-19 11:40:31 +0100

    Add JumpPad sound

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,15 +4,19 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] private float jumpSpeed;
     private Animator animator;
+    private AudioSource audioSource;
 
     private void Start()
     {
         animator = GetComponent<Animator>(); 
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<PlayerMovement>() != null)
         {
+            audioSource.Play();
+
             Rigidbody rb = other.GetComponent<Rigidbody>();
             Vector3 vel = rb.velocity;
             vel.y = jumpSpeed;
